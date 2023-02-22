e94286381cfea5593b0ff09e8715f0fa7cf485f0
Pablo Eguilaz <pabloeguilazperez@gmail.com>
2023-02-23 00:04:17 +0100

    Update

--- main.js
+++ main.js
@@ -10,20 +10,19 @@ if (title == "Middle-Point") {
 const body = document.getElementsByTagName("body")
 
 const div = document.createElement("div")
-div.setAttribute("class", "menu_popover")
+div.setAttribute("style", "position: absolute; top: 45px; right: 10px; background: #282828; width: 220px; border-radius: 10px; padding: 10px; color: #d6d3d0; display: none")
 div.setAttribute("id", "menu")
-div.setAttribute("style", "display: none")
 body[0].appendChild(div)
 const nav = document.createElement("nav")
-nav.setAttribute("class", "nav")
+nav.setAttribute("style", "width: 100%; height: 40px; display: flex; align-items: center; background-color: #282828; color: white; font-family: SF-Bold; font-size: larger; position: absolute; top: 0px;")
 nav.setAttribute("id", "nav")
 body[0].appendChild(nav)
 
 const navbar = document.getElementById("nav")
 navbar.innerHTML = `
-    <span class="app_name"><a href="https://pabloep17.github.io/story/">Middle-Point</a></span>
-    <span class="center max-width">${title != "Middle-Point" ? title : ""}</span>
-    ${see_menu == true ? `<span class="menu" id="menu_view"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M32 96v64h448V96H32zm0 128v64h448v-64H32zm0 128v64h448v-64H32z"></path></svg></span>` : ""}    
+    <span style="position: absolute; left: 10px;"><a style="text-decoration: none; color: white" href="https://nafdez.github.io/middle-point-interactive/">Middle-Point</a></span>
+    <span style="display: flex;justify-content: center; align-items: center; width: 100%;">${title != "Middle-Point" ? title : ""}</span>
+    ${see_menu == true ? `<span style="position: absolute; right: 10px; cursor: pointer;" id="menu_view"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M32 96v64h448V96H32zm0 128v64h448v-64H32zm0 128v64h448v-64H32z"></path></svg></span>` : ""}    
 `
 
 const view = localStorage.getItem("views")
